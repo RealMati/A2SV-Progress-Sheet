efcad226f2cc7d79925a6c768bc1324aead1b7ff
RealMati <133161281+RealMati@users.noreply.github.com>
2023-12-06 23:35:12 +0300

    Added solution for Majority Element II

--- A2SV-Week-1/majority-element-ii.py
+++ A2SV-Week-1/majority-element-ii.py
@@ -1,16 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        import math
-        target=math.floor(len(nums)/3)
+        from collections import Counter
+        count=Counter(nums)
         res=[]
-        ct=0
-        nums.sort()
-        for i,num in enumerate(nums):
-            if i==0 or (i>0 and num!=nums[i-1]):
-                if ct>target: res.append(nums[i-1])
-                ct=1
-            else:
-                ct+=1
-        if ct>target: res.append(nums[-1])
+        for num in count:
+            if count[num]>(len(nums)/3):
+                res.append(num)
         return res
         
