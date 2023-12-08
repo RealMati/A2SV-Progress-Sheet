fd590e744cf7713fe56650683f655d600728a441
RealMati <133161281+RealMati@users.noreply.github.com>
2023-12-08 19:40:34 +0300

    Added solution for Distance Between Bus Stops

--- A2SV-Week-1/distance-between-bus-stops.py
+++ A2SV-Week-1/distance-between-bus-stops.py
@@ -1,8 +1,14 @@
 class Solution:
     def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
-        summ=sum(distance)
-        if start>destination:
-            start,destination=destination,start
-        d=sum(distance[start:destination])
-        return min(summ-d,d)
+        
+        total_distance = sum(distance)
+
+        #swapping if the start is greater than the destination
+        if start > destination:
+            start, destination = destination, start
+
+        direct_path = sum(distance[start:destination])
+        indirect_path = total_distance - direct_path
+
+        return min(indirect_path, direct_path)
         
