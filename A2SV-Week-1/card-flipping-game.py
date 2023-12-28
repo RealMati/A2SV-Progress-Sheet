8341eb915db7c395959c95a3b5cf722a10e431fb
RealMati <133161281+RealMati@users.noreply.github.com>
2023-12-28 12:46:08 +0300

    Added solution for Card Flipping Game

---
+++ A2SV-Week-1/card-flipping-game.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def flipgame(self, fronts, backs):
+        """
+        :type fronts: List[int]
+        :type backs: List[int]
+        :rtype: int
+        """
+                    
+        
+
+        sameFrontandBack=set()
+
+        min_num=float('inf')
+
+        for idx,num in enumerate(fronts):
+
+
+                if backs[idx]==num:
+                    sameFrontandBack.add(num)
+        
+        for idx,num in enumerate(fronts):
+            if num not in sameFrontandBack:
+                min_num=min(min_num,num)
+        for idx,num in enumerate(backs):
+            if num not in sameFrontandBack:
+                min_num=min(min_num,num)
+
+        if min_num==float('inf'):
+            return 0
+        else:
+            return min_num 
+
+        
