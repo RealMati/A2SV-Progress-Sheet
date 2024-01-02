24ed883639fa705a2857fde380e1676ca35b0353
RealMati <133161281+RealMati@users.noreply.github.com>
2024-01-02 19:22:48 +0300

    Added solution for Two Sum II - Input Array Is Sorted

---
+++ A2SV phase 2/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l=0
+        r=len(numbers)-1
+        while l<r:
+            summ=numbers[l]+numbers[r]
+            if summ<target:
+                l+=1
+            elif summ>target:
+                r-=1
+            else:
+                return [l+1,r+1]
