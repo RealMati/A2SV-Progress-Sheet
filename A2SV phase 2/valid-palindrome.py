44229a664bfe125165d55fac479277dc25b6fafe
RealMati <133161281+RealMati@users.noreply.github.com>
2024-01-03 19:47:12 +0300

    Added solution for Valid Palindrome

---
+++ A2SV phase 2/valid-palindrome.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l=0
+        r=len(s)-1
+
+        while l<r:
+            if s[l].isalnum() and s[r].isalnum():
+                if s[l].lower()!=s[r].lower():
+                    return False
+                l+=1
+                r-=1
+            elif s[l].isalnum()==False:
+                l+=1
+            elif s[r].isalnum()==False:
+                r-=1
+                
+        return True
+            
+
+
