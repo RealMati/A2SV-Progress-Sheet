e93fc22cef29c8868c23177ebbd75addc3a8f33b
Mati M <133161281+RealMati@users.noreply.github.com>
2024-01-26 23:45:02 +0300

    Added solution for Max Consecutive Ones III

---
+++ A2SV phase 2/max-consecutive-ones-iii.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def longestOnes(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        l=zeroCt=maxx=0
+
+        for r in range(len(nums)):
+            if nums[r]:
+                continue
+
+            zeroCt+=1
+
+            if zeroCt>k:
+                maxx=max(maxx,r-l)
+                while zeroCt>k:
+                    if nums[l]:
+                        l+=1
+                    else:
+                        zeroCt -= 1
+                        l+=1
+                maxx=max(maxx,r-l+1)
+             
+        maxx=max(maxx, len(nums)-l)
+
+        return maxx
