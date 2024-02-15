7fa18b7c872f85b849cd8ca18808773d255a57dd
Mati M <133161281+RealMati@users.noreply.github.com>
2024-02-15 03:21:36 +0300

    Add solution for Merge Intervals

---
+++ A2SV-Camp-1/leetcode/merge-intervals.py
@@ -0,0 +1,14 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        res=[]
+
+        for i in range(len(intervals)-1):
+            if intervals[i][1]>=intervals[i+1][0]:
+                intervals[i+1][0]=intervals[i][0]
+                intervals[i+1][1]=max(intervals[i+1][1],intervals[i][1])
+            else:
+                res.append(intervals[i])
+        
+        res.append(intervals[-1])
+        return res
