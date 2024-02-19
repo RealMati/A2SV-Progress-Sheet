82a17c6efcbe128f80577414766baccf8c535e46
Mati M <133161281+RealMati@users.noreply.github.com>
2024-02-19 11:26:00 +0300

    Add solution for Daily Temperatures

--- A2SV-Camp-1/leetcode/daily-temperatures.py
+++ A2SV-Camp-1/leetcode/daily-temperatures.py
@@ -1,15 +1,12 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        res=[]
+        res=[0]*len(temperatures)
         stack=[]
-        dic={}
 
         for idx,num in enumerate(temperatures):
             while stack and num>temperatures[stack[-1]]:
-                dic[stack[-1]] = idx-stack[-1]
+                res[stack[-1]] = idx-stack[-1]
                 stack.pop()
             stack.append(idx)
         
-        for i in range(len(temperatures)):
-            res.append(dic.get(i,0))
         return res
