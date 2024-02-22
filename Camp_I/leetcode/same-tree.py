5a689c1c38162cd12d53b3ff4eeadfd769a88e9e
Mati M <133161281+RealMati@users.noreply.github.com>
2024-02-22 18:53:16 +0300

    Add solution for Same Tree

--- Camp_I/leetcode/same-tree.py
+++ Camp_I/leetcode/same-tree.py
@@ -6,25 +6,11 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-        count = 0
-        def issame(p,q):
-            nonlocal count
-            if not p and q:
-                count+=1
-                return
-            if not q and p:
-                count+=1
-                return
-            if not q and not p:
-                return
-            if p.val!=q.val:
-                count+=1
-            issame(p.left,q.left)
-            issame(p.right,q.right)
-            return
-        issame(p,q)
-        if count:
+        if (not p and q) or (not q and p):
             return False
-        return True
+        if (not p and not q):
+            return True
+        if p.val!=q.val:
+            return False
+        return self.isSameTree(p.right,q.right) and self.isSameTree(p.left,q.left)
+     
