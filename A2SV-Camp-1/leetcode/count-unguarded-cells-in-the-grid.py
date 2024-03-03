b61305e0dccdfbc37ac5e0d52c59d19f4270079e
Mati Milkessa <133161281+RealMati@users.noreply.github.com>
2024-03-03 14:50:46 +0300

    Add solution for Count Unguarded Cells in the Grid

---
+++ A2SV-Camp-1/leetcode/count-unguarded-cells-in-the-grid.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+        space = [['.' for _ in range(n)] for _ in range(m)]
+        empty = m*n - len(guards) - len(walls)
+        for guard in guards:
+            space[guard[0]][guard[1]] = 'o'
+
+        for wall in walls:
+            space[wall[0]][wall[1]] = 'o'
+
+        dir=[(0, -1), (0, 1), (1, 0), (-1, 0)]
+        def walk(r, c):
+            for dx, dy in dir:
+                row, col = r + dy, c + dx
+
+                while row >= 0 and row < m and col >= 0 and col < n:
+                    if space[row][col] == '.':
+                        space[row][col] = 'g'
+                        nonlocal empty
+                        empty -= 1
+                    elif space[row][col] == 'o':
+                        break
+                    row, col = row + dy, col + dx
+
+        for guard in guards:
+            walk(guard[0], guard[1])
+
+        return empty
