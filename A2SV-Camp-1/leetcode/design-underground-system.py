e6caee88455752c017e95bf78ca99458520ec764
Mati Milkessa <133161281+RealMati@users.noreply.github.com>
2024-03-03 21:44:46 +0300

    Add solution for Design Underground System

---
+++ A2SV-Camp-1/leetcode/design-underground-system.py
@@ -0,0 +1,24 @@
+class UndergroundSystem:
+
+    def __init__(self):
+        self.trips=defaultdict(int)
+        self.tripCt=defaultdict(int)
+        self.inboard={}
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.inboard[id]=[stationName, t]
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        self.trips[self.inboard[id][0], stationName]+= t-self.inboard[id][1]
+        self.tripCt[self.inboard[id][0], stationName]+=1
+        del self.inboard[id]
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        return self.trips[startStation, endStation]/self.tripCt[startStation, endStation]
+
+
+# Your UndergroundSystem object will be instantiated and called as such:
+# obj = UndergroundSystem()
+# obj.checkIn(id,stationName,t)
+# obj.checkOut(id,stationName,t)
+# param_3 = obj.getAverageTime(startStation,endStation)
