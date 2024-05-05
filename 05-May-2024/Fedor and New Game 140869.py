2a9e680cc5590804783a4034a68038924605061c
Mati Milkessa <133161281+RealMati@users.noreply.github.com>
2024-05-06 00:42:36 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 05-May-2024/Fedor and New Game 140869.py
@@ -0,0 +1,20 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n, m, k = list(map(int, input().split()))
+nums=[]
+for i in range(m):
+    num = int(input())
+    nums.append(num)
+    # print(bin(num))
+
+check = int(input())
+res=0
+for num in nums:
+    diff=0
+    for i in range(21):
+        if num&1<<i!=check&1<<i:
+            diff+=1
+    if diff<=k:
+        res+=1
+print(res)
+
