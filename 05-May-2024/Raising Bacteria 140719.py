da37a047b4ce0c493e7fcec9c599224b0ab87852
Mati Milkessa <133161281+RealMati@users.noreply.github.com>
2024-05-05 20:43:55 +0300

    Created 'Raising Bacteria' by A2SV Hub

---
+++ 05-May-2024/Raising Bacteria 140719.py
@@ -0,0 +1,15 @@
+# Problem: Raising Bacteria - https://codeforces.com/contest/579/problem/A
+
+num = int(input())
+
+ct=0
+for i in range(32):
+    if num&1<<i: 
+        ct+=1
+print(ct)
+
+
+
+    
+    
+    
