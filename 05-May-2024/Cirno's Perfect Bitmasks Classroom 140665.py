ee0d891a0087cc77bfffa8f0998966799e1d1c6c
Mati Milkessa <133161281+RealMati@users.noreply.github.com>
2024-05-05 18:20:34 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 05-May-2024/Cirno's Perfect Bitmasks Classroom 140665.py
@@ -0,0 +1,35 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+size = int(input())
+for i in range(size):
+    num = int(input())
+    ans=0
+    zero=one=None
+    length=ct=0
+    for i in range(32):
+        if num&1<<i: 
+            length=i+1
+            ct+=1
+
+    
+    for i in range(length):
+        if num&1<<i:
+            one=i
+            break
+    for i in range(length):
+        if num&1<<i==0:
+            zero=i
+            break
+    ans |= 1<<one
+    if ct==1:
+        if zero!=None:
+            ans|=1<<zero
+        else:
+            ans|=1<<length
+
+    print(ans)
+
+
+    
+    
+    
