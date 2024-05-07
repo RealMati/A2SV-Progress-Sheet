8cc59c44514db92734283f45f958ad2ceadf3da1
Mati Milkessa <133161281+RealMati@users.noreply.github.com>
2024-05-07 23:12:00 +0300

    Created 'B - tourist orz!' by A2SV Hub

---
+++ A2SV G5 - Contest #15 07-May-2024/B - tourist orz! 142911.py
@@ -0,0 +1,11 @@
+# Problem: B - tourist orz! - https://codeforces.com/gym/522079/problem/B
+
+size = int(input())
+for i in range(size):
+    n, k = list(map(int, input().split()))
+    nums= list(map(int, input().split()))
+
+    maxx=0
+    for num in nums:
+        maxx=max(maxx, num|k)
+    print(maxx)
