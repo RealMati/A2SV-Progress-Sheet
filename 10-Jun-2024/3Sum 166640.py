32607e3b1c00513efd857463f9db5097ec405b32
Mati Milkessa Ensermu <133161281+RealMati@users.noreply.github.com>
2024-06-11 00:32:34 +0300

    Created '3Sum' by A2SV Hub

---
+++ 10-Jun-2024/3Sum 166640.py
@@ -0,0 +1,30 @@
+# Problem: 3Sum - https://leetcode.com/problems/3sum/description/
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        sett=set()
+        for i in range(len(nums)):
+            target=nums[i]*-1
+            l=i+1
+            r=len(nums)-1
+
+            while l<r:
+                if l==i:
+                    l+=1
+                    continue
+                elif r==i:
+                    r-=1
+                    continue 
+                summ=nums[l]+nums[r]
+                if summ==target:
+                    sett.add((nums[i],nums[l],nums[r]))
+                    l+=1
+                    r-=1        
+                elif summ<target:
+                    l+=1
+                else:
+                    r-=1
+        print(sett)
+        return sett
+            
