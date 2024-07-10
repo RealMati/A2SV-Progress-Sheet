7af6ee2da8c380fe2ab063b58d24597647b2c137
Mati Milkessa Ensermu <133161281+RealMati@users.noreply.github.com>
2024-07-11 02:02:24 +0300

    Created 'Frequency Tracker' by A2SV Hub

---
+++ 10-Jul-2024/Frequency Tracker 178766.py
@@ -0,0 +1,63 @@
+# Problem: Frequency Tracker - https://leetcode.com/problems/frequency-tracker/description/
+
+class FrequencyTracker(object):
+
+    def __init__(self):
+        self.freq=defaultdict(int)
+        self.count=defaultdict(int)
+    def add(self, number):
+        """
+        :type number: int
+        :rtype: None
+        """
+        #adds it to frequency
+        self.freq[number]+=1
+
+        #removes itfrom the prev grp
+        if self.freq[number]-1:
+            self.count[self.freq[number]-1]-=1
+
+         #adds it to the prev grp
+        self.count[self.freq[number]]+=1
+
+
+        
+
+    def deleteOne(self, number):
+        """
+        :type number: int
+        :rtype: None
+        """
+        if self.freq[number]:
+            self.freq[number]-=1
+
+
+            #removes it from the prev grp
+
+            self.count[self.freq[number]+1]-=1
+
+            #adds it to the new grp
+            self.count[self.freq[number]]+=1
+            
+            
+
+    def hasFrequency(self, frequency):
+        """
+        :type frequency: int
+        :rtype: bool
+        """
+        if self.count[frequency]: return True
+
+        return False
+        
+        
+
+
+        
+
+
+# Your FrequencyTracker object will be instantiated and called as such:
+# obj = FrequencyTracker()
+# obj.add(number)
+# obj.deleteOne(number)
+# param_3 = obj.hasFrequency(frequency)
