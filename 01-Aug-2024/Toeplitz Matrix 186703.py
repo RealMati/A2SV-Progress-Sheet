605ad55873357403bb527a973a5a90673b12c9ff
Mati Milkessa Ensermu <133161281+RealMati@users.noreply.github.com>
2024-08-02 00:31:05 +0300

    Created 'Toeplitz Matrix' by A2SV Hub

---
+++ 01-Aug-2024/Toeplitz Matrix 186703.py
@@ -0,0 +1,14 @@
+# Problem: Toeplitz Matrix - https://leetcode.com/problems/toeplitz-matrix/
+
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        for vert in range(1,len(matrix)):
+            for hor in range(1,len(matrix[0])):
+                if matrix[vert][hor]!=matrix[vert-1][hor-1]:
+                    return False
+        return True
+
+
+                    
+
+        
